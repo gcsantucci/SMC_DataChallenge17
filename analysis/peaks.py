2ce5bacbc66deaae07a908833fc3f3ee95ccb8a5
Gabriel Santucci <gabrielsantucci@gmail.com>
2017-08-22 14:58:00 -0400

    analysis + paper

---
+++ analysis/peaks.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def GetPeaks(x, mph=None, mpd=1, threshold=0, kpsh=False, show=False, ax=None, scale=[]):
+
+    x = np.atleast_1d(x).astype('float64')
+    dx = x[1:] - x[:-1]
+    ire = np.where((np.hstack((dx, 0)) <= 0) & (np.hstack((0, dx)) > 0))[0]
+    ind = np.unique(ire)
+    # first and last values of x cannot be peaks
+    if ind.size and ind[0] == 0:
+        ind = ind[1:]
+    if ind.size and ind[-1] == x.size-1:
+        ind = ind[:-1]
+    # remove peaks < minimum peak height
+    if ind.size and mph is not None:
+        ind = ind[x[ind] >= mph]
+    # remove peaks - neighbors < threshold
+    if ind.size and threshold > 0:
+        dx = np.min(np.vstack([x[ind]-x[ind-1], x[ind]-x[ind+1]]), axis=0)
+        ind = np.delete(ind, np.where(dx < threshold)[0])
+    # detect small peaks closer than minimum peak distance
+    if ind.size and mpd > 1:
+        ind = ind[np.argsort(x[ind])][::-1]  # sort ind by peak height
+        idel = np.zeros(ind.size, dtype=bool)
+        for i in range(ind.size):
+            if not idel[i]:
+                # keep peaks with the same height if kpsh is True
+                idel = idel | (ind >= ind[i] - mpd) & (ind <= ind[i] + mpd) \
+                    & (x[ind[i]] > x[ind] if kpsh else True)
+                idel[i] = 0  # Keep current peak
+        # remove the small peaks and sort back the indices by their occurrence
+        ind = np.sort(ind[~idel])
+    ind = np.array([i for i in ind if scale[i] < 6])
+
+    if show:
+        _plot(x, mph, mpd, threshold, ax, ind)
+        if scale.size:
+            _plot2(scale, x, ind, mph, mpd, threshold)
+
+    return ind
+
+def _plot2(x, y, ind, mph, mpd, threshold):
+    _, ax = plt.subplots(1, 1, figsize=(8, 4))
+    ax.plot(x, y, 'b', lw=1)
+    if ind.size:
+        label = 'peak'
+        label = label + 's' if ind.size > 1 else label
+        ax.plot(x[ind], y[ind], '+', mfc=None, mec='r', mew=2, ms=8,
+                label='{} {}'.format(ind.size, label))
+        ax.legend(loc='best', framealpha=.5, numpoints=1)
+    ax.set_xlim(x.min()-.1, x.max()+.1)
+    ax.set_ylim(0, y.max()+100)
+    ax.set_xlabel('d spacing (A)', fontsize=14)
+    ax.set_ylabel('Intensity', fontsize=14)
+    mode = 'Peak detection'
+    ax.set_title('{} (mph={}, mpd={}, threshold={})'.format(mode, mph, mpd, threshold))
+    plt.show()
+
+def _plot(x, mph, mpd, threshold, ax, ind):
+    if ax is None:
+        _, ax = plt.subplots(1, 1, figsize=(8, 4))
+        ax.plot(x, 'b', lw=1)
+        if ind.size:
+            label = 'peak'
+            label = label + 's' if ind.size > 1 else label
+            ax.plot(ind, x[ind], '+', mfc=None, mec='r', mew=2, ms=8,
+                    label='%d %s' % (ind.size, label))
+            ax.legend(loc='best', framealpha=.5, numpoints=1)
+        ax.set_xlim(-.02*x.size, x.size*1.02-1)
+        ymin, ymax = x[np.isfinite(x)].min(), x[np.isfinite(x)].max()
+        yrange = ymax - ymin if ymax > ymin else 1
+        ax.set_ylim(ymin - 0.1*yrange, ymax + 0.1*yrange)
+        ax.set_xlabel('Entry #', fontsize=14)
+        ax.set_ylabel('Intensity', fontsize=14)
+        mode = 'Peak detection'
+        ax.set_title("%s (mph=%s, mpd=%d, threshold=%s)"
+                     % (mode, str(mph), mpd, str(threshold)))
+        plt.show()
---
+++ analysis/visuals.py
@@ -0,0 +1,96 @@
+from scipy.optimize import curve_fit
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec, pylab
+
+def gaus(x, x0, sigma):
+    return (1/(sigma*np.sqrt(2*np.pi))) * np.exp( (pow(x-x0, 2)) / (-2*pow(sigma, 2)) )
+
+def gaus_b(x, A, x0, sigma, b):
+    return A*gaus(x, x0, sigma) + b
+
+def LSE(x, y, a, x0, sigma, b, err):
+    return sum( [(pow(gaus_b(ix, a, x0, sigma, b) - iy, 2))/err for ix, iy in zip(x, y)])/len(y)
+
+def Norm(X):
+    m = X.mean()
+    s = X.std()
+    return (X-m)/s
+
+def GetBkg(y):
+    mu = y.mean()
+    std = y.std()
+    y = np.array([i for i in y if i < mu + 3*std])
+    return y.mean(), y.std()
+
+def GetSeed(x, y):    
+    mean = x[np.argmax(y)]
+    peak = y.max()
+    minFW = 1000
+    for arg, i in enumerate(y):
+        if abs(i - peak/2) < minFW:
+            minFW = abs(i - peak/2)
+            argFW = arg
+    sigma = abs(x[argFW]-mean) / 2.355    
+    area = sigma*peak
+    return area, mean, sigma
+
+def GetVars(x, y, inds, bkg):
+    areas = []
+    means = []
+    sigmas = []
+    bkgs = []
+    for i, ind in enumerate(inds):
+        imin = ind-50 if ind>50 else 0
+        imax = ind+50 if ind<len(x)-50 else len(x)-1
+        xfit, yfit = x[imin:imax], y[imin:imax]
+        area, mean, sigma = GetSeed(xfit, yfit)
+        popt, pcov = curve_fit(gaus_b, xfit, yfit, p0=[area, mean, sigma, bkg])
+        area, mean, sigma, bkg = popt
+        areas.append(round(area, 3))
+        means.append(round(mean, 3))
+        sigmas.append(round(sigma, 3))
+        bkgs.append(round(bkg, 2))
+    return areas, means, sigmas, bkgs
+
+def TestFit(x, y, temp):
+    d0 = round(min(x), 2)
+    d = round(max(x), 2)
+    
+    area, mean, sigma = GetSeed(x, y)
+    bkg, err = GetBkg(y)
+    popt, pcov = curve_fit(gaus_b, x, y, p0=[area, mean, sigma, bkg])
+
+    area, mean, sigma, bkg = popt
+    Yhat = np.array([gaus_b(ix, area, mean, sigma, bkg) for ix in x])
+    res = Yhat - y
+    chi2 = LSE(x, y, area, mean, sigma, bkg, err)
+    
+    fig = plt.figure(figsize=(8,7))
+    gs = gridspec.GridSpec(2, 1,  height_ratios=[5, 1]) 
+
+    ax1 = plt.subplot(gs[0])
+    plt.plot(x, y, 'k.:', label='data T = {} K'.format(temp))
+    plt.plot(x, gaus_b(x, *popt), 'r+:', label='fit T = {} K'.format(temp))
+    plt.legend()
+    plt.title('Peak at {} - {} A for T = {} K'.format(d0, d, temp))
+    frame = pylab.gca()
+    frame.axes.get_xaxis().set_ticks([])
+    plt.ylabel('Intensity')
+    plt.annotate('Fit Results for T = {} K:\nA = {}\n$\mu$ = {}\n$\sigma$ = {}\nChi2={}'.format(temp,
+                                                                                       round(area,2),
+                                                                                       round(mean,2),
+                                                                                       round(sigma,4),
+                                                                                       round(chi2, 2) ),
+                 xy=(d0, max(y)*.7))
+
+    ax2 = plt.subplot(gs[1])
+    plt.errorbar(x, res, yerr=err, fmt='b.')
+    plt.plot([d0, d], [0, 0])
+    plt.xlabel('dspacing (Angstrom)')
+    plt.ylabel('Residues')
+    plt.subplots_adjust(hspace=.003)
+    plt.show()
+
+
+    return
